0700356ca29bd2ba47f3b31c4e0c790743e7f7ac
Mikhail Zakharov <zmey20000@yahoo.com>
2023-06-15 20:59:34 +0200

    Base64.py update

--- Base64.py
+++ Base64.py
@@ -23,8 +23,8 @@ class Base64:
             ot2 = ord(dec[tptn + 2]) if tptn + 2 <= length - 1 else 0
             # Output sextets
             st0 = self.BASE64_ALPHABET[ot0 >> 2]
-            st1 = self.BASE64_ALPHABET[((ot0 & 0x3) << 4) + (ot1 >> 4)]
-            st2 = '=' if not ot1 else self.BASE64_ALPHABET[((ot1 & 0xF) << 2) + (ot2 >> 6)]
+            st1 = self.BASE64_ALPHABET[(ot0 & 0x3) << 4 | ot1 >> 4]
+            st2 = '=' if not ot1 else self.BASE64_ALPHABET[(ot1 & 0xF) << 2 | ot2 >> 6]
             st3 = '=' if not ot2 else self.BASE64_ALPHABET[ot2 & 0x3F]
 
             enc += st0 + st1 + st2 + st3
@@ -38,12 +38,12 @@ class Base64:
             # Input sextets
             st0 = self.BASE64_ALPHABET.index(enc[qttn])
             st1 = self.BASE64_ALPHABET.index(enc[qttn + 1])
-            st2 = self.BASE64_ALPHABET.index(enc[qttn + 2]) if enc[qttn + 2] != '=' else ''
-            st3 = self.BASE64_ALPHABET.index(enc[qttn + 3]) if enc[qttn + 3] != '=' else ''
+            st2 = self.BASE64_ALPHABET.index(enc[qttn + 2]) if enc[qttn + 2] != '=' else 0
+            st3 = self.BASE64_ALPHABET.index(enc[qttn + 3]) if enc[qttn + 3] != '=' else 0
             # Output octets
-            ot0 = chr((st0 << 2) + (st1 >> 4))
-            ot1 = chr(((st1 & 0xF) << 4) + (st2 >> 2)) if st2 else ''
-            ot2 = chr(((st2 & 0x3) << 6) + (st3 & 0x3F)) if st3 else ''
+            ot0 = chr(st0 << 2 | st1 >> 4)
+            ot1 = chr((st1 & 0xF) << 4 | st2 >> 2) if st2 else ''
+            ot2 = chr((st2 & 0x3) << 6 | st3 & 0x3F) if st3 else ''
 
             dec += ot0 + ot1 + ot2
 
